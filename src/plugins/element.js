5c46ab21c3cc57c5c7063d7d2d3273293a29bcff
anjing529 <532868948@qq.com>
2020-02-26 00:18:49 +0800

    'login_2nd'

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,13 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
 
+// 按需导入
+import {Form,FormItem} from 'element-ui'
+import {Input} from 'element-ui'
+
 Vue.use(Button)
+
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
